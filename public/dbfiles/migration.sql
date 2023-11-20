1c3d5855a59db13b392f3b99fb63759136a85cb6
Thiem Huynh <thiem.huynh0@gmail.com>
2023-11-20 15:49:12 -0500

    basic sql migrations

--- public/dbfiles/migration.sql
+++ public/dbfiles/migration.sql
@@ -1,1 +1,11 @@
-CREATE TABLE person 
+DROP TABLE IF EXISTS  person;
+
+CREATE TABLE person(
+    id SERIAL PRIMARY KEY,
+    fname VARCHAR,
+    lname VARCHAR
+);
+
+INSERT INTO person (fname, lname) VALUES 
+('Ryan', 'Hill'),
+('Sarah', 'Love');
